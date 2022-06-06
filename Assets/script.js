2d7400bd8b720cc4acb4695636c970cc3392921c
Naweed Nabi <naweednabi@hotmail.com>
2022-06-06 16:04:31 -0700

    added functions to store information to local storage

--- Assets/script.js
+++ Assets/script.js
@@ -1,18 +1,31 @@
-// Get the modal
+var humanName = document.getElementById("name");
+var emailAddy = document.getElementById("email");
+var furryName = document.getElementById("furry");
+var submitBtn = document.getElementById("submitBtn");
 var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
 var btn = document.getElementById("adoptBtn");
-
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
+
+// function executed on click of submit that stores pertinent data to local storage.
+
+submitBtn.addEventListener("click", function() {
+    var potentialClient = {
+                humanName: humanName.value,
+                emailAddy: emailAddy.value,
+                furryName: furryName.value.trim()
+  };
+  
+  localStorage.setItem("potentialClient", JSON.stringify(potentialClient));
+  return;
+  });
+
+
+
 btn.onclick = function() {
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
