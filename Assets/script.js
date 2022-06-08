23767a5a5c3bab72c51d4b3b59dbd0e38159753b
Ashley Rodriguez <rodash87@gmail.com>
2022-06-07 21:07:33 -0700

    updated JS for button forward and backward, added cat pics, updated JS, CSS, HTMl

--- Assets/script.js
+++ Assets/script.js
@@ -5,6 +5,8 @@ var submitBtn = document.getElementById("submitBtn");
 var modal = document.getElementById("myModal");
 var btn = document.getElementById("adoptBtn");
 var span = document.getElementsByClassName("close")[0];
+var catFact = document.getElementById("catFacts");
+var catBtn = document.getElementById("catBtn");
 
 
 // function executed on click of submit that stores pertinent data to local storage.
@@ -31,7 +33,9 @@ span.onclick = function() {
 }
 
 
-fetch('https://cat-fact.herokuapp.com/facts', {
+
+catBtn.addEventListener ("click", function() {
+  fetch('https://cat-fact.herokuapp.com/facts', {
   method: 'GET', //GET is the default.
   credentials: 'same-origin', // include, *same-origin, omit
   redirect: 'follow', // manual, *follow, error
@@ -40,6 +44,45 @@ fetch('https://cat-fact.herokuapp.com/facts', {
     return response.json();
   })
   .then(function (data) {
+    randomNum = Math.floor(Math.random() * 5)
     console.log(data);
-    
-  });
+    var Fact = document.createElement('p')
+    Fact.textContent = data[randomNum].text
+    catFact.innerHTML = '';
+    catFact.appendChild(Fact);
+  })
+});
+
+function displaySlides(num) {
+  var x;
+  var slides = document.getElementsByClassName("catSlide");
+  if (num > slides.length) { currentIndex = 1 }
+  if (num < 1) { currentIndex = slides.length }
+  for (x = 0; x < slides.length; x++) {
+      slides[x].style.display = "none";
+  }
+  slides[currentIndex - 1].style.display = "block";
+}
+
+function setSlides(num) {
+  displaySlides(currentIndex += num);
+}
+
+var currentIndex = 1;
+displaySlides(currentIndex);
+
+function setSlides(num) {
+  displaySlides(currentIndex += num);
+}
+
+function display(num) {
+  var x;
+  var catSlides = document.getElementsByClassName("catSlide");
+  if (num > slides.length) { currentIndex = 1}
+  if (num < 1) { currentIndex = slides.length}
+  for (x = 0; x < slides.length; X++){
+        slides[x].style.display = "none";
+  }
+  slides[currentIndex - 1].style.display = "block";
+
+}
